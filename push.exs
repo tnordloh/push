f32c12e5dea6db2414ccb7c2b53bb33746284260
claytonflesher <claytonflesher@gmail.com>
2017-06-21 11:09:39 -0500

    broken write

--- push.exs
+++ push.exs
@@ -4,25 +4,26 @@ defmodule Push do
 
   def build_archive(treeish_value, directory) do
     System.cmd("git",
-               ["archive", treeish_value],
-               into: IO.binstream(:stdio, :line),
-               cd: directory)
+      ["archive", treeish_value],
+      cd: directory)
+      |> elem(0)
   end
 
-
-  def send_archive({io_stream, 0}) do
-    file = File.stream!("testing.tar.gz")
-
-    io_stream
-    |> Stream.into(file)
-    |> Stream.run
-  end
-  def send_archive({_io_stream, _}) do
-    IO.puts "THIS IS BAD"
+  def send_archive(archive_data, destination_name, host) do
+    :ssh.start
+    {:ok, channel, _connection} = :ssh_sftp.start_channel(
+      host,
+      [user: 'ec2-user', silently_accept_hosts: true]
+    )
+    {:ok, file} = :ssh_sftp.open(channel, destination_name, [:write, :binary])
+    :ok = :ssh_sftp.write(channel, file, archive_data)
+    :ssh_sftp.close(channel, file)
+    :ssh_sftp.stop_channel(channel)
   end
 
 end
 
 "master"
-|> Push.build_archive("hanabi_umbrella")
-|> Push.send_archive
+|> Push.build_archive("../hanabi_umbrella")
+|> Push.send_archive('hanabi_umbrella.tar.gz', 'some_host')
+
